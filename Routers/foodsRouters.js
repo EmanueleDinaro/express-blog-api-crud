84617a87b55f873c4557e3dd1bc56b3fb0379ea1
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-01-26 21:35:53 +0100

    feat: add food controller

--- Routers/foodsRouters.js
+++ Routers/foodsRouters.js
@@ -1,58 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const foodsData = require("../data/foodsData")
+const foodsController = require('../controller/foodsController')
+const foods = require('../data/foodsData')
 
 //INDEX
-router.get('/', )
+router.get('/', foodsController.index)
 
 //SHOW
-router.get('/:id', (req, res) => {
-    const id = Number(req.params.id)
-    
-    const food = foodsData.find(food => food.id === id)
-
-    if(!food){
-        res.status(404)
-        res.json({
-            error: "404 Not Found",
-            message: "L'alimento non è stato trovato"
-        })
-    }
-    res.json(food)
-})
+router.get('/:id', foodsController.show)
 
 //STORE
-router.post('/', (req, res) => {
-    res.send('Creazione di un nuovo alimento')
-})
+router.post('/', foodsController.store)
 
 //UPDATE
-router.put('/:id', (req, res) => {
-    res.send('Modifica integrale di un alimento')
-})
+router.put('/:id', foodsController.update)
 
 //MODIFY
-router.patch('/:id', (req, res) => {
-    res.send('Modifica parziale di un alimento')
-})
+router.patch('/:id', foodsController.patch)
 
 //DELETE
-router.delete('/:id', (req, res) => {
-    const id = Number(req.params.id)
-    
-    const food = foodsData.find(food => food.id === id)
-
-    if(!food){
-        res.status(404)
-        
-        return res.json({
-            error: "404 Not Found",
-            message: "L'alimento non è stato trovato"
-        })
-    }
-    foodsData.splice(foodsData.indexOf(food), 1)
-
-    res.sendStatus(204)
-})
+router.delete('/:id', foodsController.destroy)
 
 module.exports = router
--- controller/foodsController.js
+++ controller/foodsController.js
@@ -1,7 +1,60 @@
 const foodsData = require('../data/foodsData')
 
+//index
 const index = (req, res) => {
     res.json(foodsData)
 }
 
-module.exports = { index }
+
+//show
+const show = (req, res) => {
+    const id = Number(req.params.id)
+    
+    const food = foodsData.find(food => food.id === id)
+
+    if(!food){
+        res.status(404)
+        res.json({
+            error: "404 Not Found",
+            message: "L'alimento non è stato trovato"
+        })
+    }
+    res.json(food)
+}
+
+//store
+const store = (req, res) => {
+    res.send('Creazione di un nuovo alimento')
+}
+
+//update
+const update = (req, res) => {
+    res.send('Modifica integrale di un alimento')
+}
+
+//modify
+const patch = (req, res) => {
+    res.send('Modifica parziale di un alimento')
+}
+
+//delete
+const destroy = (req, res) => {
+    const id = Number(req.params.id)
+    
+    const food = foodsData.find(food => food.id === id)
+
+    if(!food){
+        res.status(404)
+        
+        return res.json({
+            error: "404 Not Found",
+            message: "L'alimento non è stato trovato"
+        })
+    }
+    foodsData.splice(foodsData.indexOf(food), 1)
+
+    res.sendStatus(204)
+}
+
+module.exports = { index, show, store, update, patch, destroy }
+
